aed096927f94847dbed020436db40f52a7acaf78
Andrew  Perlis <ajperlis07@gmail.com>
2021-01-07 01:01:30 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -10,9 +10,7 @@ def assembled_aoh
   assembled_aoh = [
     DON_G,
     JOELLE_VD, 
-    PAT_M,
-    KATE_G,
-    BRUCE_G]
+    PAT_M,]
     assembled_aoh
     
   # Build an array that contains (or, "nests") the constants into a single
@@ -20,6 +18,7 @@ def assembled_aoh
 end
 
 def literal_aoh
+  
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
